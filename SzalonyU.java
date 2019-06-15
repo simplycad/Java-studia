cdb6f86c46d80bc95bf2cbde653088b8108569c7
Grzegorz Janik <45257536+simplycad@users.noreply.github.com>
2019-06-15 16:34:10 +0200

    Add files via upload

---
+++ SzalonyU.java
@@ -0,0 +1,38 @@
+
+ import java.util.*;
+ import java.io.*;
+
+public class SzalonyU {
+
+
+    public static void main(String[] args)
+	throws IOException{
+
+ 	FileReader fin = new FileReader("IMIONA.IN");
+ 	Scanner src = new Scanner (fin);
+
+	String imie;
+	TreeSet<String>LI=new TreeSet<String>();
+
+
+
+	while(src.hasNext()){
+
+		imie=src.next();
+	    imie=imie.replace("-","");
+	    imie=imie.toLowerCase();
+
+	    imie = imie.substring(0,1).toUpperCase()+ imie.substring(1);
+		LI.add(imie);
+
+
+
+	}
+	System.out.println(LI);
+    }
+}
+
+
+
+
+
